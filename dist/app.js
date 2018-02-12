b26723e77e089aa4745ddd779a28351ebe5d6a6e
Elizabeth Kelen <elizabethkelen@liz.local>
2018-02-12 15:11:35 +0100

    ugh

--- dist/app.js
+++ dist/app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use('/data', express.static(path.join(__dirname, 'data')));
 
 app.get('/', function (req, res) {
-  return res.sendFile('/data/index.html');
+  return res.sendFile('/data/index.html', { root: __dirname });
 });
 
 app.use('/api/v1', router);
