17ea54e50504af3995827ccc802793a0c5692e93
Elizabeth Kelen <elizabethkelen@liz.local>
2018-02-12 15:02:07 +0100

    hmm

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use('/data', express.static(path.join(__dirname, 'data')))
 
 app.get('/', (req, res) => {
-  return res.send('Welcome to the RWS card data API...')
+  return res.sendFile('data/index.html')
 })
 
 app.use('/api/v1', router)
